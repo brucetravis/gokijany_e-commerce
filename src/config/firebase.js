7bb1ee85d847817b6fb6d2801cb9a5682bb4e308
BRUCE TRAVIS <122971716+brucetravis@users.noreply.github.com>
2025-10-05 10:22:17 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -10,14 +10,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAm1CS5KpgQjh_F3m6zwddjeC1yXQL6z7Y",
-  authDomain: "gokijany-mobile-app.firebaseapp.com",
-  databaseURL: "https://gokijany-mobile-app-default-rtdb.firebaseio.com",
-  projectId: "gokijany-mobile-app",
-  storageBucket: "gokijany-mobile-app.appspot.com",
-  messagingSenderId: "396041560862",
-  appId: "1:396041560862:web:f43a0c5ae48563c3131f0f",
-  measurementId: "G-16HBJ0ZLT1"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -35,4 +35,5 @@ export const db = getDatabase(app)
 export const auth = getAuth(app)
 
 // const analytics = getAnalytics(app);
+
 
